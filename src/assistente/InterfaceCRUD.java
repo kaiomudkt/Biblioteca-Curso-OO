a826a00d52b104d38a86e3bf20dc6dc9ed983fc8
root <kaiomudkt@gmail.com>
2019-05-16 13:42:53 -0400

    comentario CRUD

--- src/assistente/InterfaceCRUD.java
+++ src/assistente/InterfaceCRUD.java
@@ -1,5 +1,11 @@
 package assistente;
-
+/**
+ * CRUD (acrónimo do inglês Create, Read, Update e Delete) 
+ * são as quatro operações básicas 
+ * (criação, consulta, atualização e destruição de dados) 
+ * utilizadas em bases de dados relacionais (RDBMS) 
+ * fornecidas aos utilizadores do sistema.
+ */
 public interface InterfaceCRUD {
 
     public boolean contem(String chave);
